99d14108b39abee93216db215ee800d7e5d0e913
dougreichard <doug.reichard+github@gmail.com>
2024-02-26 16:38:50 -0500

    Updated to 0.9.8d

--- script.py
+++ script.py
@@ -1,11 +1,51 @@
 import sbslibs
 from  sbs_utils.handlerhooks import *
 from sbs_utils.gui import Gui
-from sbs_utils.pymast.pymaststorypage import PyMastStoryPage
-from story import Story
+from sbs_utils.mast.maststorypage import StoryPage
+from sbs_utils.mast.maststory import MastStory
 
-class MyStoryPage(PyMastStoryPage):
+from sbs_utils.mast.mast import Mast
+from sbs_utils.mast.pollresults import PollResults
+from sbs_utils.mast.label import label
+from story import start_server, start_client
+
+from sbs_utils.procedural.gui import gui_reroute_server, gui_row, gui_button, gui, gui_section, gui_text
+from sbs_utils.procedural.execution import AWAIT
+
+
+class Story(MastStory):
+    def ____init__(self):
+        super.__init__()
+        self.main = self.main_gui
+
+    def test(self):
+        print("Press")
+
+    @label()
+    def main_gui(self):
+        #yield PollResults.OK_RUN_AGAIN
+        # gui_reroute_server(server_start)
+        gui_section("area: 20,20,50,50")
+        gui_button("Hello", on_message=self.test)
+        gui_row()
+        gui_text("Hello")
+        
+        # g = gui()
+        # while not g.done():
+        #     yield PollResults.OK_RUN_AGAIN
+        yield AWAIT(gui())
+        
+
+
+class SimpleAiPage(StoryPage):
     story = Story()
+    # main_server = story.main_gui
+    main_server = start_server
+    main_client = start_client
 
-Gui.server_start_page_class(MyStoryPage)
-Gui.client_start_page_class(MyStoryPage)
+
+
+Mast.include_code = True
+
+Gui.server_start_page_class(SimpleAiPage)
+Gui.client_start_page_class(SimpleAiPage)
--- story.py
+++ story.py
@@ -1,144 +1,141 @@
 import sbslibs
 import sbs
 from  sbs_utils.handlerhooks import *
-from sbs_utils.pymast.pymaststory import PyMastStory
-from sbs_utils.pymast.pymasttask import label
-from sbs_utils import query
-from sbs_utils.objects import PlayerShip, Npc
+from sbs_utils.mast.label import label
+from sbs_utils.procedural import query
+from sbs_utils.procedural import gui
+from sbs_utils.procedural import roles
+from sbs_utils.procedural.execution import AWAIT, jump, set_variable, get_variable, set_shared_variable, get_shared_variable
+from sbs_utils.procedural.timers import delay_sim
+from sbs_utils.procedural.spawn import npc_spawn, player_spawn
 
 
-class Story(PyMastStory):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+start_text = "This is a start project for mast"
 
-        self.start_text = "This is a start project for mast"
+@label()
+def start_server():
+    #
+    # Create the player ship so the clients see it
+    # This is a simple script that has one playable ship
+    #
+    sbs.create_new_sim()
 
-    @label()
-    def start_server(self):
-        #
-        # Create the player ship so the clients see it
-        # This is a simple script that has one playable ship
-        #
-        sbs.create_new_sim()
+
+    # create a player ship
+    artemis =  query.to_id(player_spawn(0,0,-500, "Artemis", "tsn", "tsn_battle_cruiser"))
+    set_shared_variable("artemis", artemis)
+    # Client screen need to be assigned to a ship
+    # this assigns the main screen to artemis
+    sbs.assign_client_to_ship(0,artemis)
+
+    # Queue an area to place gui content
+    gui.gui_section("area:2,20,80,35;")
+    # Queue a text gui content
+    gui.gui_text(f""" {start_text}""")
+
+    # Present the queued Gui Content
+    # as well as a set of buttons
+    #
+    # Buttons have text, and the can specify 
+    # a label to run when selected
     
-
-        # create a player ship
-        self.artemis =  query.to_id(PlayerShip().spawn(self.sim, 0,0,-500, "Artemis", "tsn", "tsn_battle_cruiser"))
-        # Client screen need to be assigned to a ship
-        # this assigns the main screen to artemis
-        sbs.assign_client_to_ship(0,self.artemis)
-
-        # Queue an area to place gui content
-        self.gui_section("area:2,20,80,35;")
-        # Queue a text gui content
-        self.gui_text(f""" {self.start_text}""")
-
-        # Present the queued Gui Content
-        # as well as a set of buttons
-        #
-        # Buttons have text, and the can specify 
-        # a label to run when selected
-        yield self.await_gui({
-            "Start": self.start
-        })
+    yield AWAIT(gui.gui({
+         "Start": start
+    }))
+    
+@label()
+def end_game():
+    global start_text
+    # 
+    # Check for end game
+    #
+    # no more players
+    
+    players = roles.role("__PLAYER__")
+    if len(players) == 0:
+        start_text = "You lost"
+        sbs.pause_sim()
+        yield jump(start_server)
+    #
+    # No more enemies
+    #
+    raiders = roles.role("raider")
+    if len(raiders) == 0:
+        start_text = "You Won"
+        print(f"end_game {start_text}")
+        sbs.pause_sim()
+        yield jump(start_server)
         
-    @label()
-    def end_game(self):
-        # 
-        # Check for end game
-        #
-        # no more players
-        
-        players = query.role("__PLAYER__")
-        if len(players) == 0:
-            self.start_text = "You lost"
-            sbs.pause_sim()
-            yield self.jump(self.start_server)
-        #
-        # No more enemies
-        #
-        raiders = query.role("raider")
-        if len(raiders) == 0:
-            self.start_text = "You Won"
-            print(f"end_game {self.start_text}")
-            sbs.pause_sim()
-            yield self.jump(self.start_server)
-            
-        yield self.delay(5)
-        yield self.jump(self.end_game)
+    yield AWAIT(delay_sim(5))
+    yield jump(end_game)
 
-    @label()
-    def start(self):
-        # Create the world here
+@label()
+def start():
+    # Create the world here
 
-        # Create a space station
-        ds1 = Npc().spawn(self.sim, 1000,0,1000, "DS1", "tsn", "starbase_command", "behav_station")
+    # Create a space station
+    ds1 = npc_spawn( 1000,0,1000, "DS1", "tsn", "starbase_command", "behav_station")
 
-        # Create an enemy
-        k001 = Npc().spawn(self.sim, -1000,0,1000, "K001", "raider", "kralien_battleship", "behav_npcship")
+    # Create an enemy
+    k001 = npc_spawn(-1000,0,1000, "K001", "raider", "kralien_battleship", "behav_npcship")
 
-        sbs.resume_sim()
-        # jump to the end game label
-        yield self.jump(self.end_game)
+    sbs.resume_sim()
+    # jump to the end game label
+    yield jump(end_game)
 
 
-    @label()
-    def start_client(self):
-        #
-        # This handles the change client button to return to the select_console
-        #
-        # event change_console:
-        #     ->select_console
-        # end_event
-        #
-        # Default the console to helm
-        #
-        self.task.console_select = "helm"
-        yield self.jump(self.select_console)
+@label()
+def start_client():
+    #
+    # This handles the change client button to return to the select_console
+    #
+    # event change_console:
+    #     ->select_console
+    # end_event
+    #
+    # Default the console to helm
+    #
+    set_variable("console_select",  "helm")
+    yield jump(select_console)
 
-    @label()
-    def select_console(self):
+@label()
+def select_console():
 
-        # Queue an area to play content
-        self.gui_section("area:2,20,80,35;")
+    # Queue an area to play content
+    gui.gui_section("area:2,20,80,35;")
 
-        # Queue up some text
-        self.gui_text("""Select your console""")
+    # Queue up some text
+    gui.gui_text("""Select your console""")
 
-        # Queue another area to place content
-        self.gui_section("area: 85,50, 99,90;row-height:200px")
-        # Queue a set of radio button for the consoles
-        console_radio = self.gui_radio("helm,weapons,comms,science,engineering", self.task.console_select, True)
-        # Add a new row to the area
-        self.gui_row("row-height: 30px;")
-        # Queue blank spot to separate the accept button
-        self.gui_blank()
-        # Add another row for the accept button
-        # This time forcing the height of the row
-        self.gui_row("row-height: 30px;")
-        # Queue up the accept button
-        # The button takes the text and a label to called
-        # when the button is pressed
-        self.gui_button("accept", self.console_selected)
+    # Queue another area to place content
+    gui.gui_section("area: 85,50, 99,90;row-height:200px")
+    # Queue a set of radio button for the consoles
+    gui.gui_vradio("helm,weapons,comms,science,engineering", var="console_select")
+    # Add a new row to the area
+    gui.gui_row("row-height: 30px;")
+    # Queue blank spot to separate the accept button
+    gui.gui_blank()
+    # Add another row for the accept button
+    # This time forcing the height of the row
+    gui.gui_row("row-height: 30px;")
+    # Queue up the accept button
+    # The button takes the text and a label to called
+    # when the button is pressed
+    gui.gui_button("accept", jump=console_selected)
 
-        # This is a function to watch for changes when the gui is shown
-        # This one watches for changes in the radio buttons
-        def on_message(sim, event):
-            if event.sub_tag.startswith(console_radio.tag):
-                self.task.console_select = console_radio.value
-                return True
-
-        # Show the queued gui content (no buttons)
-        # And this is where you specify the on_message function so it is called
-        yield self.await_gui(on_message=on_message)
-        
-    @label()
-    def console_selected(self):
-        # This is called when the accept button is pressed
-        # Assign the gui client to Artemis
-        sbs.assign_client_to_ship(self.task.page.client_id,self.artemis)
-        # Queue showing the widgets for the console selected 
-        self.gui_console(self.task.console_select)
-        # Present the queued gui content
-        self.await_gui()
-        
+    
+    # Show the queued gui content (no buttons)
+    # And this is where you specify the on_message function so it is called
+    yield AWAIT(gui.gui())
+    #yield AWAIT(gui.gui({"accept": console_selected}))
+    
+@label()
+def console_selected():
+    # This is called when the accept button is pressed
+    # Assign the gui client to Artemis
+    sbs.assign_client_to_ship(get_variable("client_id"),get_variable("artemis"))
+    # Queue showing the widgets for the console selected 
+    gui.gui_console(get_variable("console_select"))
+    # Present the queued gui content
+    yield AWAIT(gui.gui())
+    
